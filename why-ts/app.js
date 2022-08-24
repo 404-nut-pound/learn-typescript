9f94672dcb190d4d18943823d4836f6ddf6913f6
khs <souls4523@gmail.com>
2022-08-25 00:59:38 +0900

    - 자바스크립트 데이터 타입 정의

--- why-ts/app.js
+++ why-ts/app.js
@@ -6,6 +6,23 @@ var username = document.querySelector('#username');
 var email = document.querySelector('#email');
 var address = document.querySelector('#address');
 
+/**
+ * 데이터 타입 정의
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} street
+ */
+
+/**
+ * @typedef {object} Address
+ * @property {string} city
+ * @property {{lat: string, lng: string}} geo
+ * @property {string} street
+ * @property {string} suite
+ * @property {string} zipcode
+ */
+
 // user data
 var user = {};
 
@@ -16,6 +33,9 @@ function startApp() {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerHTML = user.name;
+      email.innerHTML = user.email;
+      address.innerHTML = user.address.street;
     })
     .catch(function (error) {
       console.log(error);
